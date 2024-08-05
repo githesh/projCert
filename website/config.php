ec1aeed69b3643322e711024dd28936fe15b83bf
GITHESH GOPIANTH <39765882+githesh@users.noreply.github.com>
2024-08-05 09:24:11 +0530

    Update config.php

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website For PWC',
+        'name' => 'Simple PHP Website For EDU',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
